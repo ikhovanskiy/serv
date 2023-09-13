cc59618208d86c1c915c23ff3679655db513604c
ikhovanskiy <khovanskiyi@rambler.ru>
2023-09-13 08:33:27 +0300

    task: 2 week

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const http = require('node:http')
 
+function task(x){
+    this.x = x;
+    return x * Math.pow(this.x, 2);
+}
 
 http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' , 'Access-Control-Allow-Origin': '*', 'X-Author':'itmo371919'});
-    res.end('itmo371919');
+    res.writeHead(200, { 'Content-Type': 'text/plain; charset=UTF-8' , 'Access-Control-Allow-Origin': '*'});
+    if (req.url === '/login/') {
+        res.end('itmo371919');
+    } else if (req.url === '/sample/') { 
+        res.end(task.toString())
+    } else {
+        res.end();
+    }
+   
+    
 }).listen(3000)
